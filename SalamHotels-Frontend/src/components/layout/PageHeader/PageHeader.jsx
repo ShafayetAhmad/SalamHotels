0dc19d04954e91dbd0da4af1405b864959d4cb4a
ShafayetAhmad <shafayet.ahmad1@gmail.com>
2024-03-05 01:43:29 +0600

    page header made responsive

--- SalamHotels-Frontend/src/components/layout/PageHeader/PageHeader.jsx
+++ SalamHotels-Frontend/src/components/layout/PageHeader/PageHeader.jsx
@@ -2,8 +2,10 @@ const PageHeader = ({ pageTitle, pageDescription }) => {
   return (
     <div className="bg-[rgb(4,36,49)] h-72 text-white mb-2 flex items-center ">
       <div className="max-w-7xl mx-auto ">
-        <h2 className="m-0 text-5xl w-full text-center">{pageTitle}</h2>
-        <p className="text-2xl">{pageDescription}</p>
+        <h2 className="m-0 lg:text-5xl text-3xl w-full text-center">
+          {pageTitle}
+        </h2>
+        <p className="text-2xl lg:text-left text-center">{pageDescription}</p>
       </div>
     </div>
   );
